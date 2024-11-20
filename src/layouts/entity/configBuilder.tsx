42de70de885e85cb187ceb7a3a866e39afeb2926
tTakaneUni <ttakane@unisrv.jp>
2024-11-20 17:57:03 +0900

    Update configBuilder.tsx

--- src/layouts/entity/configBuilder.tsx
+++ src/layouts/entity/configBuilder.tsx
@@ -107,7 +107,7 @@ export const configBuilder: (
     },
     // The entity language profiles that documents will be generated for.
     localization: {
-      locales: ["en"],
+      locales: ["ja"],
     },
   },
   // Specifies the field to use for URL paths when serving locally.
